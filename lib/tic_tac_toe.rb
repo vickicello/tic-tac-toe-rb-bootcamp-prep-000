32938a909257faaa746a53ab1b05f5406ed8550f
Vicki Hamm <vicki.cello@gmail.com>
2018-05-14 04:49:52 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -57,8 +57,13 @@ def turn_count(board)
     if board == "X" || board == "O"
       counter += 1
     end
+ end
+counter
 end
 
 def current_player(board)
+  turn_count(board).even? ?  "X":"O"
+ 
+end
   
 
