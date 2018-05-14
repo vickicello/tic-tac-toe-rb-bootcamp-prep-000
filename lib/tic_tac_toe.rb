e02e7f899b15e25af880546a517d2050ab0a5f4e
Vicki Hamm <vicki.cello@gmail.com>
2018-05-14 04:46:35 +0000

    automatically backed up by learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -39,4 +39,18 @@ def valid_move?(board, index)
  end
 end
 
+def turn(board)
+  puts "Please enter 1-9:"
+  input = gets
+  index = input_to_index(input)
+    if valid_move?(board, index)
+      move(board, index)
+      display_board(board)
+    else
+     turn(board)
+   end
+end
+
+def turn_count(board)
+  
 
